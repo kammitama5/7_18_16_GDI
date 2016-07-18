b31834e1ecf94f06e4b5530c0a8f8a4acf27e302
kammitama5 <krystal.maughan@gmail.com>
2016-07-18 09:41:41 -0700

    2nd commit

--- mycode.js
+++ mycode.js
@@ -43,3 +43,20 @@ for (var i = 0; i < myFavouriteThings.length; i++)
 
 //======================================================================================
 
+//Function of Array of Objects
+
+var Things = {
+
+	age: 27,
+	furColor: 'chinchilla',
+	likes: ['money', 'travel', 'paying people to do my shit for me'],
+	birthday: {month: 7, day: 4, year: 1989}
+}
+
+function describeThings(thing){
+	console.log('I am ' + thing.age + ' years old and I like ' + thing.likes[2]);
+}
+
+describeThings(Things);
+
+//=======================================================================================
